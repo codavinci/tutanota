9ea6a20198f335ff706e41efe0b4051bcc9f2c59
vaf <vaf@tutao.de>
2021-03-02 14:26:18 +0100

    user spam reports

--- src/api/common/TutanotaConstants.js
+++ src/api/common/TutanotaConstants.js
@@ -679,6 +679,7 @@ export const MailReportType = Object.freeze({
 	PHISHING: "0",
 	SPAM: "1",
 })
+export type MailReportTypeEnum = $Values<typeof MailReportType>
 
 export const DnsRecordValidation = Object.freeze({
 	OK: "✓",
@@ -687,7 +688,6 @@ export const DnsRecordValidation = Object.freeze({
 export type DnsRecordValidationTypeEnum = $Values<typeof DnsRecordValidation>;
 
 
-export type MailReportTypeEnum = $Values<typeof MailReportType>
 export const CalendarAttendeeStatus = Object.freeze({
 	/** invite is not sent yet */
 	ADDED: "0",
